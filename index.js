306e90b5c43cd331af574c726a8a980bc4b3412e
Maksym <likeran2@gmail.com>
2019-03-29 21:02:47 +0200

    some minor changes in PrintToPage class

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-document.getElementById("get-full-data").addEventListener("click", getData);
-document.getElementById("get-one-employee").addEventListener("click", getData);
+document.getElementById("get-full-data").addEventListener("click", getAllData);
+document.getElementById("get-one-employee").addEventListener("click", getDataByName);
 document.getElementById("clear-list").addEventListener("click", removeDataFromList);
+
 let idCounter = 0;
 let buttonId = "btn-id_";
 
 class GetData {
 	constructor() {
 		this.URL_SERVER = "http://dummy.restapiexample.com/api/v1/employees";
+		this.arrOfEmployee = [];
 	}
 
 	async employeeData() {
 		const response = await fetch(this.URL_SERVER);
-		const data = await response.json();
-		return data;
+		this.data = await response.json();
+		this.arrOfEmployee = this.data;
+		return this.arrOfEmployee;
 	}
 }
 
@@ -23,49 +26,48 @@ class PrintToPage extends GetData {
 		this.searchName = document.querySelector(".search-employee__input").value;
 		this.employeeList = document.querySelector(".employees-container__list");
 		this.detailsButtonClass = "list__button";
-		this.handleDetailsButtonClick = this.handleDetailsButtonClick.bind(this);
-		this.clickedButton = this.employeeList.addEventListener("click", this.handleDetailsButtonClick);
-		this.getEmployeeByName();
+		this.employeeName = "list__employee-name";
+		this.emptyFile = "Employee not found";
+		this.createsParagraph = this.createsParagraph.bind(this);
+		this.addCreatedElementsToList = this.addCreatedElementsToList.bind(this);
+		this.detailsButtonModifier = this.detailsButtonModifier.bind(this);
+		this.clickedButton = this.employeeList.addEventListener("click", this.detailsButtonModifier);
 	}
 
-	handleDetailsButtonClick(event) {
-		const target = this.getEventTarget(event);
-		if (target.classList.contains(this.detailsButtonClass)) {
-			return target.closest(".employees-container__list-element").classList.toggle("employees-container__list-element--active");
+	detailsButtonModifier(event) {
+		console.log(event);
+		if (event.target.classList.contains(this.detailsButtonClass)) {
+			event.target.closest(".employees-container__list-element").classList.toggle("employees-container__list-element--active");
 		}
 	}
 
-	getEventTarget(e) {
-  		e = e || window.event;
-  		return e.target || e.srcElement;
-	}
-
 	async getEmployeeByName() {
-		const data = await this.employeeData();
-		data.forEach(employee => {
-			const name = employee.employee_name;
-			if (name.toLowerCase().includes(this.searchName)) {
-				this.createListElement(name);
-			}
-		});
+		await this.employeeData();
+		console.log(this.arrOfEmployee);
+		if (this.searchName === "") {
+			this.addCreatedElementsToList(this.createsParagraph(this.emptyFile), undefined);
+		} else {
+			this.arrOfEmployee.forEach(employee => {
+				const name = employee.employee_name;
+				if (name.toLowerCase().includes(this.searchName.toLowerCase())) {
+					this.addCreatedElementsToList(this.createsParagraph(name), this.createsButtonDetails());
+				}
+			});
+		}
+		return this.arrOfEmployee;
 	}
 
-	createListElement(employee) {
+	createsListItem() {
 		const employeeElement = document.createElement("li");
 		employeeElement.classList.add("employees-container__list-element");
+		return employeeElement;
+	}
 
+	createsParagraph(employee) {
 		const employeeName = document.createElement("p");
+		employeeName.classList.add(this.employeeName);
 		employeeName.textContent = employee;
-
-		const employeeDetails = document.createElement("button");
-		employeeDetails.textContent = "details";
-		employeeDetails.classList.add(this.detailsButtonClass);
-		employeeDetails.id = `${this.createId(buttonId)}`
-
-		employeeElement.appendChild(employeeName);
-		employeeElement.appendChild(employeeDetails);
-		this.employeeList.appendChild(employeeElement);
-		this.listContainer.classList.add("employees-container--visible");
+		return employeeName;
 	}
 
 	createId(elem) {
@@ -73,15 +75,52 @@ class PrintToPage extends GetData {
 		return elem + idCounter;
 	}
 
-	clearPage() {
+	createsButtonDetails() {
+		const employeeDetails = document.createElement("button");
+		employeeDetails.textContent = "details";
+		employeeDetails.classList.add(this.detailsButtonClass);
+		employeeDetails.id = `${this.createId(buttonId)}`
+		return employeeDetails;
+	}
 
+	addCreatedElementsToList(paragraph, button) {
+		const resultList = this.createsListItem();
+		if (paragraph !== undefined) {
+			resultList.appendChild(paragraph);
+		} if (button !== undefined) {
+			resultList.appendChild(button);
+		}
+		this.employeeList.appendChild(resultList);
+		return this.employeeList;
+	}
+
+	createListElement(employee) {
+		this.employeeList.appendChild(this.createsListItem(employee));
 	}
 }
 
-function getData() {
-	new PrintToPage();
+class Remover extends PrintToPage {
+	constructor() {
+		super();
+		this.clearPage();
+	}
+
+	clearPage() {
+		while (this.employeeList.firstChild) {
+			this.employeeList.removeChild(this.employeeList.firstChild);
+		}
+	}
+}
+
+function getAllData() {
+
+}
+
+function getDataByName() {
+	const newSearch = new PrintToPage();
+	newSearch.getEmployeeByName();
 }
 
 function removeDataFromList() {
-
+	new Remover();
 }
